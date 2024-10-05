bf92ce4b03024350409a2af66917d25c68e9a15d
567892143 <20211a04n5@bvrit.ac.in>
2024-10-05 09:36:40 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 // connect MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    const PORT = process.env.PORT || 8000
+mongoose.connect("mongodb://dinesh:test123@expensetrac.8jvbw.mongodb.net/?retryWrites=true&w=majority&appName=expensetrac").then(() => {
+    const PORT = 8000
     app.listen(PORT, () => {
         console.log(`App is Listening on PORT ${PORT}`);
     })
